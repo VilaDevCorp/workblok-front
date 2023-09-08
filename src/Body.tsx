101e3e22edcdda8238283bc98eb638b077e3c2f2
david.vila <davilasanvilar@gmail.com>
2023-09-09 01:50:51 +0200

    Routes lazy loading

--- src/Body.tsx
+++ src/Body.tsx
@@ -1,22 +1,19 @@
 import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
-import { Header } from './components/organism/Header';
 import { useAuth } from './hooks/useAuth';
-import { ActivitiesScreen } from './screens/ActivitiesScreen';
-import styled from 'styled-components';
 import { LoginScreen } from './screens/LoginScreen';
-import { ConfirmationModal } from './components/organism/ConfirmationModal';
 import { PlannerScreen } from './screens/PlannerScreen';
 import { VilaLoadingScreen } from './components/ui/VilaLoadingScreen';
-import { RegisterScreen } from './screens/RegisterScreen';
-import { ValidateAccountScreen } from './screens/ValidateAccountScreen';
 import { VilaSnackbar } from './components/ui/VilaSnackbar';
-import { ForgottenPasswordScreen } from './screens/ForgottenPasswordScreen';
-import { TemplatesScreen } from './screens/TemplatesScreen';
-import { StatsScreen } from './screens/StatsScreen';
 import { useMisc } from './hooks/useMisc';
-import { CircleLoader, MoonLoader, PuffLoader } from 'react-spinners';
-import { useEffect, useState } from 'react';
+import { PuffLoader } from 'react-spinners';
+import { Suspense, lazy, useEffect, useState } from 'react';
 
+const LazyActivitiesScreen = lazy(() => import('./screens/ActivitiesScreen').then((module) => ({ default: module.ActivitiesScreen })))
+const LazyRegisterScreen = lazy(() => import('./screens/RegisterScreen').then((module) => ({ default: module.RegisterScreen })));
+const LazyValidateAccountScreen = lazy(() => import('./screens/ValidateAccountScreen').then((module) => ({ default: module.ValidateAccountScreen })));
+const LazyForgottenPasswordScreen = lazy(() => import('./screens/ForgottenPasswordScreen').then((module) => ({ default: module.ForgottenPasswordScreen })));
+const LazyTemplatesScreen = lazy(() => import('./screens/TemplatesScreen').then((module) => ({ default: module.TemplatesScreen })));
+const LazyStatsScreen = lazy(() => import('./screens/StatsScreen').then((module) => ({ default: module.StatsScreen })));
 
 function Body() {
 
@@ -35,18 +32,18 @@ function Body() {
     <div className='w-full h-full md:h-screen bg-background-900' >
       <BrowserRouter>
         {authInfo.isCompletedLoad === true ?
-          <>
+          <Suspense fallback={<VilaLoadingScreen />}>
             <Routes>
               <Route path="/" element={<PlannerScreen />} />
               <Route path="/login" element={<LoginScreen />} />
-              <Route path="/register" element={<RegisterScreen />} />
-              <Route path="/activities" element={<ActivitiesScreen />} />
-              <Route path="/templates" element={<TemplatesScreen />} />
-              <Route path="/stats" element={<StatsScreen />} />
-              <Route path="/validate/:userMail" element={<ValidateAccountScreen />} />
-              <Route path="/recover-password" element={<ForgottenPasswordScreen />} />
+              <Route path="/register" element={<LazyRegisterScreen />} />
+              <Route path="/activities" element={<LazyActivitiesScreen />} />
+              <Route path="/templates" element={<LazyTemplatesScreen />} />
+              <Route path="/stats" element={<LazyStatsScreen />} />
+              <Route path="/validate/:userMail" element={<LazyValidateAccountScreen />} />
+              <Route path="/recover-password" element={<LazyForgottenPasswordScreen />} />
             </Routes>
-          </>
+          </Suspense>
           :
           <VilaLoadingScreen />
         }
