e51a442a11514e40eee1e27d854da56601b7b7c0
david.vila <davilasanvilar@gmail.com>
2023-09-16 13:23:23 +0200

    Now textinput can grow to fill space

--- src/components/ui/VilaTextInput.tsx
+++ src/components/ui/VilaTextInput.tsx
@@ -18,7 +18,7 @@ export function VilaTextInput({ type = 'text', disabled = false, ...props }: Pro
     const [isFocused, setIsFocused] = useState(false)
 
     return (
-        <div className="flex flex-col gap-2">
+        <div className="flex flex-col gap-2 flex-grow">
             <div className={`flex gap-2 items-center px-3 h-[50px] py-3 border border-transparent bg-background-300 rounded-lg text-lightFont-500 ${isFocused ? ' bg-background-400 !border-primary-500 ' : ''} ${disabled ? 'brightness-50 ' : 'hover:bg-background-400 '}`} >
                 {props.icon && props.iconAlignment !== 'right' && <VilaIcon type={props.icon} />}
                 <input className="bg-transparent focus:outline-none w-full" onFocus={() => setIsFocused(true)} maxLength={props.maxChars} onBlur={() => setIsFocused(false)} onChange={(e) => props.setValue(e.target.value)} value={props.value} type={type} disabled={disabled} />
