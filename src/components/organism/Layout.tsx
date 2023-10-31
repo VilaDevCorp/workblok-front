a2f985ff77308c83925e3bcdcbee0897d7c384a6
davilasanvilar <83314751+davilasanvilar@users.noreply.github.com>
2023-10-31 22:20:44 +0100

    Removed unnecessary background styles

--- src/components/organism/Layout.tsx
+++ src/components/organism/Layout.tsx
@@ -25,12 +25,12 @@ export function Layout({
     //Public Layout
     isPublic ? (
       <main
-        className={`min-h-full flex-col md:h-auto md:min-h-full w-full p-4 flex h-screen backdrop-blur-sm items-center justify-center bg-[url("/front-vertical-min.jpg")] md:bg-[url("/front-image-min.jpg")] bg-cover`}
+        className={`min-h-full flex-col md:h-auto md:min-h-full w-full p-4 flex h-screen backdrop-blur-sm items-center justify-center`}
       >
         {children}
       </main>
     ) : (
-        //Private layout
+      //Private layout
       <main
         className={`w-full flex flex-col h-full max-w-[500px] m-auto p-8 gap-8`}
       >
@@ -40,11 +40,10 @@ export function Layout({
   ) : (
     //Not logged in layout
     <div
-      className={`min-h-full flex  flex-col items-center bg-center md:min-h-full h-screen backdrop-blur-sm w-full px-4 py-4 bg-[url("/front-vertical-min.jpg")] 
-            md:bg-[url("/front-image-min.jpg")] bg-cover '} `}
+      className={`min-h-full flex  flex-col items-center bg-center md:min-h-full h-screen w-full px-4 py-4`}
     >
       <main
-        className={`flex backdrop-brightness-[20%] backdrop-blur-sm p-8 rounded-lg h-full md:h-auto justify-center items-center py flex-col gap-6 m-auto`}
+        className={`flex p-8 rounded-lg h-full md:h-auto justify-center items-center py flex-col gap-6 m-auto`}
       >
         <Logo className="mb-10" />
         <Typography>{"You need an account to view this page"}</Typography>
